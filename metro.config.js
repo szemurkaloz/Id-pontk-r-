db985569320427f43fc546f4d77cfac994c3e66c
Kovács József <szemurkaloz@gmail.com>
2021-11-23 15:14:41 +0100

    console log false

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
     transformer: {
         getTransformOptions: async () => ({
@@ -11,3 +12,28 @@ module.exports = {
         sourceExts: ['jsx', 'js', 'ts', 'tsx'], //add here
     },
 };
+*/
+const { getDefaultConfig } = require('metro-config')
+
+module.exports = (async () => {
+    const {
+        resolver: { assetExts },
+    } = await getDefaultConfig()
+    return {
+        transformer: {
+            getTransformOptions: async () => ({
+                transform: {
+                    experimentalImportSupport: false,
+                    inlineRequires: false,
+                },
+            }),
+            // Remove all console logs in production...
+            minifierConfig: {
+                compress: {drop_console: true}
+            }
+        },
+        resolver: {
+            assetExts: [...assetExts],
+        },
+    }
+})()
